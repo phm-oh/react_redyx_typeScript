e4b0c94eedc16da1dde840017fd2958c37db3d42
phanumet choompun <oh@udvc.ac.th>
2023-05-10 13:23:18 +0700

    innit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+
+
+// import App from './App.tsx'
 import './index.css'
+import HomePage from './pages/home-page.tsx'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      {/* <App /> */}
+      <HomePage/>
+    </ChakraProvider>
   </React.StrictMode>,
 )
